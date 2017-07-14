bfb3707509ebede148238b811d8f7bf397878efd
aroy <aroyiem@gmail.com>
2017-07-14 10:39:56 +0530

    Delete itemCategory.sql

--- itemCategory.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO itemcategory (`code`, `description`, `status`, `createdBy`) VALUES ('English', 'English', 3, 1);
-INSERT INTO itemcategory (`code`, `description`, `status`, `createdBy`) VALUES ('Hindi', 'Hindi', 3, 1);
-INSERT INTO itemcategory (`code`, `description`, `status`, `createdBy`) VALUES ('Sanskrit', 'Sanskrit', 3, 1);
